db1c0ee0d48462243f6acf95db4fcb47dc9861dc
benzkittisak <benzkittisak@stv.ac.th>
2022-06-22 12:30:59 +0700

    Initial Commit

---
+++ quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quizzler
+//
+//  Created by Kittisak Panluea on 22/6/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
